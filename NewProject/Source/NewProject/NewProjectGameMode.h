bcc0270ee32cedb2e133822b0c0dedd73f93dff3
unknown <Tech@C401IT19.SCT.local>
2016-01-20 11:20:39 -0500

    first commit

---
+++ NewProject/Source/NewProject/NewProjectGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "NewProjectGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEWPROJECT_API ANewProjectGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
